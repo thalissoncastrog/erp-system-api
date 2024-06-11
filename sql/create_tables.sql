b7d4457a90b2605cd82a7863a650e2eb4569a0f7
Thalisson Castro <adaothalisson@hotmail.com>
2024-06-10 23:34:47 -0300

    created sql creation commands to each entity.

---
+++ sql/create_tables.sql
@@ -0,0 +1,15 @@
+CREATE TABLE cities(
+    city_id INT PRIMARY KEY IDENTITY(1,1),
+    name NVARCHAR(100) NOT NULL,
+    state NVARCHAR(50) NOT NULL,
+);
+
+CREATE TABLE clients(
+	client_id INT PRIMARY KEY IDENTITY(1,1),
+    name NVARCHAR(100) NOT NULL,
+    gender CHAR(1) NOT NULL,
+    birth_date DATE NOT NULL,
+    age INT NOT NULL,
+    city_id INT,
+    FOREIGN KEY (city_id) REFERENCES cities(city_id)
+);
