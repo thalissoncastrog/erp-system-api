327fe7d9dee899df171b1a9593829c59e61a5c2a
Thalisson Castro <adaothalisson@hotmail.com>
2024-06-10 21:08:29 -0300

    created city model

---
+++ src/SystemAPI/Models/City.cs
@@ -0,0 +1,8 @@
+namespace SystemAPI.Models;
+
+public class City
+{
+    public long Id { get; set; }
+    public required string Name { get; set; }
+    public required string State { get; set; }
+}
