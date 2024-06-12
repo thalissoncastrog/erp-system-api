669da43e180f548bdeb80aafa6faa6ac88db3bf9
Thalisson Castro <adaothalisson@hotmail.com>
2024-06-12 15:47:29 -0300

    Started DB Context

--- src/SystemAPI/Program.cs
+++ src/SystemAPI/Program.cs
@@ -3,16 +3,18 @@ using SystemAPI.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
+
 // Add services to the container.
 
 builder.Services.AddControllers();
 
-builder.Services.AddDbContext<SystemContext>(opt =>
-    opt.UseInMemoryDatabase("Registry"));
+//builder.Services.AddDbContext<SystemContext>(opt =>
+//    opt.UseInMemoryDatabase("Registry"));
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<MyDbContext>();
 
 var app = builder.Build();
 
