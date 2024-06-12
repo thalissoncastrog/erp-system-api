df076c24634906e7f0ea1bbee260b58a9433e323
Thalisson Castro <adaothalisson@hotmail.com>
2024-06-12 15:50:01 -0300

    created user model and controller

---
+++ src/SystemAPI/Models/User.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace SystemAPI.Models
+{
+    [Table("Users")]
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        public string? Name { get; set; }
+    }
+}
