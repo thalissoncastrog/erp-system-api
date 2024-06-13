374f67e9dbbfb3f2ea784d98ddd7d396887480de
Thalisson Castro <adaothalisson@hotmail.com>
2024-06-13 02:30:02 -0300

    Deleted some oldest entities

--- src/SystemAPI/Models/City.cs
+++
@@ -1,8 +0,0 @@
-namespace SystemAPI.Models;
-
-public class City
-{
-    public long Id { get; set; }
-    public required string Name { get; set; }
-    public required string State { get; set; }
-}
