0f4138cd167e7fafc475832c34e6d0e5d6368a47
Thalisson Castro <adaothalisson@hotmail.com>
2024-06-13 02:33:27 -0300

    created the `City.cs` and `Client.cs` entities

---
+++ src/SystemAPI/Models/Entities/City.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using SystemAPI.Models.Entities;
+using System.Collections.Generic;
+
+namespace SystemAPI.Models.Entities
+{
+    public class City
+    {
+        [Key]
+        public int City_Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string State { get; set; }
+
+        public ICollection<Client> Clients { get; set; } = new List<Client>();
+    }
+}
